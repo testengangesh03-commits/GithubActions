bdc04ee6dcec6755b89adaa773f79a4ce126b8a7
testengangesh03 <testengangesh03@gmail.com>
2026-03-20 12:51:10 +0530

    modified

--- tests/phone.spec.js
+++ tests/phone.spec.js
@@ -2,5 +2,5 @@
 import{test}from '@playwright/test';
 test('phonenumber',async({page})=>
 {
-    await page.locator('#phone').fill(9787943193);
+    await page.locator('#phone').fill(879876543);
 })
