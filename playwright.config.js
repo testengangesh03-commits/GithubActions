3a57fcb004202d77fba5de36841cae3679505bd1
testengangesh03 <testengangesh03@gmail.com>
2026-03-27 13:27:11 +0530

    added

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter:[ ['line'],
-    ['allure-playwright']],
+    ['allure-playwright', { outputFolder: 'allure-results' }]],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
